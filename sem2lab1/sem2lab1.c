0dc2c43c7d7c701e47d80fac224e8e856be5739f
r1kje <127132195+r1kje@users.noreply.github.com>
2023-03-06 19:58:26 +0300

    Update sem2lab1.c

--- sem2lab1/sem2lab1.c
+++ sem2lab1/sem2lab1.c
@@ -1,5 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+struct student_session {
+	int index;
+	char surname[30];
+	int  score;
+};
+
 int main() {
 	FILE *file = fopen("test_sem1.txt", "w");
 
