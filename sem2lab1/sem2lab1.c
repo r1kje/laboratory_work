a6cffc938c1cf6ef3758232ed049d24e07024be6
r1kje <127132195+r1kje@users.noreply.github.com>
2023-05-03 09:43:56 +0300

    Update sem2lab1.c

--- sem2lab1/sem2lab1.c
+++ sem2lab1/sem2lab1.c
@@ -1,45 +1,47 @@
-#include <stdio.h>
-#include <conio.h>
 #include <iostream>
 #include <fstream>
- 
+#include <string>
+
 using namespace std;
- 
-char fileName[] = "e:\\txt.txt";
- 
-void readFile()
-{
-    ifstream fileIn;
-    fileIn.open(fileName);
-    char* tmpStr = new char[1000];
-    char* tmp = new char[500];
-    tmpStr[0]=0;
-    bool comma = false;
-    while(fileIn >> tmp)
-    {           
-        strcat(tmpStr, tmp);
-        strcat(tmpStr, " ");
-        if(tmp[strlen(tmp)-1] == ',')
-            comma = true;
-        if(tmp[strlen(tmp)-1] == '.' || tmp[strlen(tmp)-1] == '!' || tmp[strlen(tmp)-1] == '?')
-        {
-            if(!comma)
-                cout << tmpStr;
-            tmpStr[0] = 0;
-            comma = false;
-        }
-    }
-    
-    fileIn.close();
-    delete[] tmpStr;
-}
- 
+
 int main()
 {
-    setlocale(LC_ALL,"Russian"); 
-    system("cls");  
-    cout << "Предложения, не содержащие запятых: " << endl << endl;
-    readFile();
-    getch();
+    ifstream file("test.txt"); // открываем файл для чтения
+
+    if (!file) // если файла не существует или не удаётся его открыть
+    {
+        cerr << "Не удалось открыть файл!" << endl;
+        return 1;
+    }
+
+    string line; // переменная для хранения считанной строки
+
+    while (getline(file, line)) // читаем строки из файла
+    {
+        int n = line.size(); // количество символов в строке
+        bool is_sentence = false; // флаг - является ли строка предложением
+
+        for (int i = 0; i < n; i++)
+        {
+            if (line[i] == '.') // если встретили точку
+            {
+                is_sentence = true; // считаем строку предложением
+                break;
+            }
+            else if (line[i] == ',') // если встретили запятую
+            {
+                is_sentence = false; // считаем строку НЕ предложением
+                break;
+            }
+        }
+
+        if (is_sentence) // если строка является предложением
+        {
+            cout << line << endl; // выводим её на экран
+        }
+    }
+
+    file.close(); // закрываем файл
+
     return 0;
 }
